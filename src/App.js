e01749457bb5fade0a9bf3f0fa4512644682aae4
Shine Ran <ranshine9@gmail.com>
2024-05-06 10:57:27 +0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Dashboard, Header, Login, Register } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <h1>React app</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import Register from "./Register";
+import Header from "./Header";
+
+export { Dashboard, Login, Register, Header };
