4894bcce2b2a988d69e0d9e24f6a6692867f694c
Shine Ran <ranshine9@gmail.com>
2024-05-31 16:48:17 +0300

    close sidebar on click outside it

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import styles from "./Dashboard.module.css";
 import { useSelector } from "react-redux";
@@ -10,13 +10,27 @@ const Dashboard = ({ children }) => {
   const { user } = useSelector((state) => state.auth);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    width > 764 ? setIsOpen(true) : setIsOpen(false);
-  }, [width]);
+  const backdropRef = useRef(null);
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    width > 764 ? setIsOpen(true) : setIsOpen(false);
+  }, [width]);
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (backdropRef.current && backdropRef.current.contains(e.target))
+        setIsOpen(false);
+    };
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
 
   useEffect(() => {
     if (!user) navigate("/login");
@@ -31,14 +45,18 @@ const Dashboard = ({ children }) => {
               ☰
             </button>
           </li>
-          <li>
+          <li onClick={toggleSidebar}>
             <Link to="allProducts">All Products</Link>
           </li>
-          <li>
+          <li onClick={toggleSidebar}>
             <Link to="addProduct">Add Product</Link>
           </li>
         </ul>
       </div>
+      {isOpen && width < 764 && (
+        <div className={styles.backdrop} ref={backdropRef}></div>
+      )}
+
       <div className={styles.content}>
         <button className={styles.toggleButton} onClick={toggleSidebar}>
           ☰
